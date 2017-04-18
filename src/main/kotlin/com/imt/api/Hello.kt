6efb4665fa35b65a3662804314345f9a289685e5
Anael CHARDAN <anael.chardan@gmail.com>
2017-04-18 10:36:56 +0200

    Initial commit for source

---
+++ src/main/kotlin/com/imt/api/Hello.kt
@@ -0,0 +1,6 @@
+package com.imt.api
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ src/test/kotlin/com/imt/api/HelloTest.kt
@@ -0,0 +1,8 @@
+package com.imt.api
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
