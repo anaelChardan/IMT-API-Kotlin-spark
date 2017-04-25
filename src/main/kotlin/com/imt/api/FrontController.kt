f8962ca62c4d86818b9807427001c1c905ca91f2
Anael CHARDAN <anael.chardan@gmail.com>
2017-04-25 09:57:39 +0200

    Add the API Limit on get

--- src/main/kotlin/com/imt/api/FrontController.kt
+++ src/main/kotlin/com/imt/api/FrontController.kt
@@ -6,6 +6,11 @@ import spark.Filter
 import spark.Spark.*
 
 val restaurantDao = RestaurantDao()
+val magicKey = "THIS_IS_A_SUPER_BADASS_MAGIC_KEY"
+
+var ipsCall: HashMap<String, Int> = HashMap()
+val limitation: Int = 4
+
 
 fun main(args: Array<String>) {
     port(3000)
@@ -15,16 +20,22 @@ fun main(args: Array<String>) {
     }))
 
     path("restaurants") {
-//        before("/*") { req, res ->
-//            res.type("application/json")
-//
-//        }
-//        after("/*") { req, res ->
-//            res.type("application/json")
-//
-//        }
-
         get("") { req, res ->
+            if (req.headers("MySuperID") == null) {
+                res.status(400)
+                return@get ""
+            }
+
+            val id = req.headers("MySuperID")
+
+            if (ipsCall.containsKey(id)) {
+                if (ipsCall.getValue(id) == limitation) {
+                    res.status(429)
+                    return@get ""
+                }
+            }
+
+            ipsCall[id] = ipsCall.getOrElse(id, { 0 }) + 1
             dataToJson(restaurantDao.restaurants.values)
         }
 
@@ -39,6 +50,12 @@ fun main(args: Array<String>) {
 
             res.status(404)
             ""
+        }
+    }
+
+    path("magic-key") {
+        get("") { req, res ->
+            dataToJson(magicKey)
         }
     }
 }
