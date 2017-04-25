a5a149228fcc83af82871a001d185b8fc9c335a2
Clément Garbay <clementgarbay@gmail.com>
2017-04-25 08:59:43 +0200

    Update MapperService

--- src/main/kotlin/com/imt/api/Service.kt
+++ src/main/kotlin/com/imt/api/Service.kt
@@ -2,24 +2,22 @@ package com.imt.api
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
-import java.io.IOException
-import java.io.StringWriter
 
 /**
  * @author Clément Garbay
  */
 class MapperService {
     companion object {
-        fun dataToJson(data: Any): String {
-            try {
-                val mapper = ObjectMapper()
-                mapper.enable(SerializationFeature.INDENT_OUTPUT)
-                val sw = StringWriter()
-                mapper.writeValue(sw, data)
-                return sw.toString()
-            } catch (e: IOException) {
-                throw RuntimeException("IOException from a StringWriter?")
-            }
+        fun <T> dataToJson(data: T): String {
+            val mapper = ObjectMapper()
+            mapper.enable(SerializationFeature.INDENT_OUTPUT)
+            return mapper.writeValueAsString(data)
+        }
+
+        inline fun <reified T> jsonToData(json: String): T {
+            val mapper = ObjectMapper()
+            mapper.enable(SerializationFeature.INDENT_OUTPUT)
+            return mapper.readValue(json, T::class.java)
         }
     }
 }
